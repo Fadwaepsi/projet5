58e31a109cef569f2ecdd918dd0e749ef7165e40
Fadwa Talik <fadwa.bayoudh@ecoles-epsi.net>
2024-04-15 14:27:18 +0200

    first commit

---
+++ test4.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+url="https://www.fossil.com/fr-fr/montres/montres-pour-femme/"
+
+response = requests.get(url)
+#print(response)
+content = response.content
+#print(content)
+soup = BeautifulSoup(content,'html')
+
+prix = soup.findAll('span',class_="value")
+print(prix)
